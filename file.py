0d08e74153e8d69652450f74d25f1e1180771f07
Sean <seanskg72@gmail.com>
2021-02-06 23:29:41 +1100

    Basic Regression method

--- file.py
+++ file.py
@@ -4,11 +4,15 @@ from sklearn.preprocessing import StandardScaler
 scale = StandardScaler()
 df = pd.read_excel('car_data.xlsx').dropna()
 
-print(len(df))
-
 X = df[['Year', 'Engine HP', 'Engine Cylinders']]
 y = df['MSRP']
 
 X = scale.fit_transform(X.to_numpy())
+print(X)
 est = sm.OLS(y, X).fit()
 print(est.summary())
+
+scaled = scale.transform([[2011, 300, 6]])
+print(scaled)
+predicted = est.predict(scaled[0])
+print(predicted)
