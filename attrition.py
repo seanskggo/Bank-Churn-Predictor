82280fdb0854b23a672504484eb56c243aa0b65a
Sean <seanskg72@gmail.com>
2021-02-08 22:54:41 +1100

    basic prediction

--- attrition.py
+++ attrition.py
@@ -16,21 +16,26 @@ from sklearn.preprocessing import StandardScaler
 ################################################################################
 
 scale = StandardScaler()
-df = pd.read_excel('customer.xlsx')
+df = pd.read_excel('customer.xlsx').replace(
+    {
+        'Attrition_Flag': {
+            'Existing Customer' : 0, 
+            'Attrited Customer' : 1
+        },
+        'Gender': {
+            'M' : 0, 
+            'F' : 1
+        }
+    }
+)
+X = df[['Customer_Age', 'Gender']]
+y = df['Attrition_Flag']
 
-X = df[['Attrition_Flag', 'Customer_Age', 'Gender']]
+X = scale.fit_transform(X.to_numpy())
+est = sm.OLS(y, X).fit()
+print(est.summary())
 
-print(X)
-
-
-# y = df['MSRP']
-
-# X = scale.fit_transform(X.to_numpy())
-# print(X)
-# est = sm.OLS(y, X).fit()
-# print(est.summary())
-
-# scaled = scale.transform([[2011, 300, 6]])
-# print(scaled)
-# predicted = est.predict(scaled[0])
-# print(predicted)
+scaled = scale.transform([[90, 1]])
+print("\n--------- Result ---------\n")
+predicted = est.predict(scaled[0])
+print(str(predicted) + "\n")
